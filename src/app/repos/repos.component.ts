c12af61fa3fbe6c5be1337eeda8b0a65897bf4b1
monginadiana <diana.ondieki@student.moringa.com>
2021-10-19 01:13:54 +0300

    styling of the navbar component

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ServicesService } from '../services/services.service';
 
 @Component({
   selector: 'app-repos',
@@ -6,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repos.component.css']
 })
 export class ReposComponent implements OnInit {
+  @ViewChild('e') findGithubReposForm: NgForm
+  repo1: string;
+  Info ;
 
-  constructor() { }
+  showRepo = false;
+
+  searchGithubRepos(){
+    this.repo1=this.findGithubReposForm.value.search;
+    console.log(this.repo1);
+    
+    this.requestRepo.getRepo(this.repo1).then(
+      (Response)=>{
+        this.Info=this.requestRepo.repoDetails;
+        console.log(this.Info);
+      },
+      (error)=>{
+        console.log(error);
+      }
+    );
+    this.showRepo=true;
+  }
+  constructor(private requestRepo:ServicesService) {}
+
+
 
   ngOnInit(): void {
   }
